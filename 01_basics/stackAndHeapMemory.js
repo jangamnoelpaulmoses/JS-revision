01fc105f19143406a33a10166be5803caa28c7e8
jangamnoelpaulmoses <jangamnoelpaulmoses@gmail.com>
2025-03-01 23:59:08 -0700

    minor

--- 01_basics/stackAndHeapMemory.js
+++ 01_basics/stackAndHeapMemory.js
@@ -27,5 +27,5 @@ let obj2 = myObj;
 console.log(obj2.name); // moses
 
 obj2.name = "jangam" 
-console.log(myObj.name) // jangam
+console.log(myObj.name) // jangam moses
 console.log(obj2.name); // jangam
